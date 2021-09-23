58b10240685a3c18957f48b5000d19ec901393d5
LawChristopherHau <Lawrencechristopherhau@gmail.com>
2021-09-23 11:43:26 -0400

    made migrations, migrated, created superuser, ran server, models needed to be made

--- findingyourway_django/settings.py
+++ findingyourway_django/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'findingyourway_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'findingyourway',
+        'USER': 'findingyourwayuser',
+        'PASSWORD': 'findingyourway',
+        'HOST': 'localhost'
     }
 }
 
